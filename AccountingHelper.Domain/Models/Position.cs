6796cc510b808bfd316bd07da81a9e9692f4de33
Maxim <maks.vichkutkin98@gmail.com>
2026-07-28 13:23:46 +0500

    refactor: make PositionId consistent

--- AccountingHelper.Domain/Models/Position.cs
+++ AccountingHelper.Domain/Models/Position.cs
@@ -4,7 +4,7 @@ namespace AccountingHelper.Domain.Models;
 
 public class Position
 {
-    public required Guid Id { get; set; }
+    public Guid Id { get; set; }
     public required string Title { get; set; }
     public string?  Description { get; set; }
     public required EmployeeGrade Grade { get; set; }
